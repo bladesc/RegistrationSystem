cd62afeba0f9233156b9bc893b9cf3f8d9136886
bladesc <bladesc@interia.pl>
2018-01-23 20:09:34 +0100

    install class

--- install.php
+++ install.php
@@ -1,3 +1,9 @@
+﻿<html>
+<head>
+<link rel="Stylesheet" type="text/css" href="css/style.css" />
+
+</head>
+   <body style="background: none">
 <?php
 
 include('settings/settings.php');
@@ -6,6 +12,14 @@ include("class/class.install.php");
    
 
 $install = new Install();
-$install->createTables();
-
+if($install->createTables())
+{
+	echo "<div class='positive_comments'>Tabele w bazie danych zostały zainstalowane pomyślnie</div>";
+}
+if($install->makeData())
+{
+	echo "<div class='positive_comments'>Dane w bazie danych zostały zainstalowane pomyślnie</div>";
+}
 ?>
+</body>
+</html>
